29dc499470bf82f0c75f6b7ffa7d58ac3451c552
Amene <layla_1380@yahoo.com>
2024-02-18 08:39:22 -0600

    home button

---
+++ index.js
@@ -0,0 +1,19 @@
+/*const btnA = document.getElementById('btnA')
+btnA.addEventListener('click', ()=>{
+    window.location.href = 'about.html'
+})
+
+const btnB= document.getElementById('btnB')
+btnB.addEventListener('click', ()=>{
+    window.location.href = 'home.html'
+})*/
+
+const btnA = document.getElementById('btnA');
+btnA.addEventListener('click', () => {
+    window.location.href = 'about.html';
+});
+
+const btnB = document.getElementById('btnB');
+btnB.addEventListener('click', () => {
+    window.location.href = 'home.html';
+});
